1f000972e89f502d3e0f3545554691bdd2fc1813
tavarozzi <tava.rozzi@gmail.com>
2015-08-26 02:13:31 -0300

    Update categorie.php

--- categorie.php
+++ categorie.php
@@ -66,7 +66,7 @@
                         <a href="edit_categorie.php?id=<?php echo (int)$cat['id'];?>" class="btn btn-warning btn-xs"  title="Edit">
                           <span class="glyphicon glyphicon-edit"></span>
                         </a>
-                        <a href="delete_categorie.php?id=<?php echo (int)$cat['id'];?>" class="btn btn-danger btn-xs"  title="Edit">
+                        <a href="delete_categorie.php?id=<?php echo (int)$cat['id'];?>" class="btn btn-danger btn-xs"  title="Delete">
                           <span class="glyphicon glyphicon-trash"></span>
                         </a>
                     </td>
